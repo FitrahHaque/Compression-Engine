e43c3518d59221fa2d2c38333d81e1d3d5aa6d84
Asif Ihtemadul Haque <asifihtemadulhaque@gmail.com>
2025-06-20 19:17:30 -0400

    converted lengths to RLE

--- compressor/flate/deflate.go
+++ compressor/flate/deflate.go
@@ -23,7 +23,6 @@ type DistanceAlphabets struct {
 		extraBits    int
 		baseDistance int
 	}
-	canonicalCode []huffman.CanonicalHuffmanCode
 }
 
 type LengthAlphabets struct {
@@ -31,12 +30,32 @@ type LengthAlphabets struct {
 		extraBits  int
 		baseLength int
 	}
-	canonicalCode []huffman.CanonicalHuffmanCode
+	keyOrder []int
 }
 
+type CodeLengthAlphabets struct {
+	alphabets map[int]struct {
+		extraBits int
+	}
+	keyOrder []int
+}
+
+type LitLengthCode struct {
+	LitLengthHuffman []huffman.CanonicalHuffmanCode
+}
+type DistanceCode struct {
+	DistanceHuffman []huffman.CanonicalHuffmanCode
+}
+type CodeLengthCode struct {
+	LengthEncoded []struct {
+		RLECode int
+		Offset  int
+	}
+	CodeLengthHuffman []huffman.CanonicalHuffmanCode
+}
 type AlphabetCode interface {
 	FindCode(value int) (int, int, error)
-	Encode(items interface{}) (map[int]huffman.CanonicalHuffmanCode, error)
+	Encode(items interface{}) ([]int, error)
 }
 
 type Token struct {
@@ -57,7 +76,11 @@ var lenAlphabets = LengthAlphabets{
 		extraBits  int
 		baseLength int
 	}{
-		257: {extraBits: 0, baseLength: 3}, 258: {extraBits: 0, baseLength: 4}, 259: {extraBits: 0, baseLength: 5}, 260: {extraBits: 0, baseLength: 6}, 261: {extraBits: 0, baseLength: 7}, 262: {extraBits: 0, baseLength: 8}, 263: {extraBits: 0, baseLength: 9}, 264: {extraBits: 0, baseLength: 10}, 265: {extraBits: 1, baseLength: 11}, 266: {extraBits: 1, baseLength: 13}, 267: {extraBits: 1, baseLength: 15}, 268: {extraBits: 1, baseLength: 17}, 269: {extraBits: 2, baseLength: 19}, 270: {extraBits: 2, baseLength: 23}, 271: {extraBits: 2, baseLength: 27}, 272: {extraBits: 3, baseLength: 31}, 273: {extraBits: 3, baseLength: 35}, 274: {extraBits: 3, baseLength: 43}, 275: {extraBits: 3, baseLength: 51}, 276: {extraBits: 3, baseLength: 59}, 277: {extraBits: 4}, 278: {extraBits: 4, baseLength: 83}, 279: {extraBits: 4, baseLength: 99}, 280: {extraBits: 4, baseLength: 115}, 281: {extraBits: 5, baseLength: 131}, 282: {extraBits: 5, baseLength: 163}, 283: {extraBits: 5, baseLength: 195}, 284: {extraBits: 5, baseLength: 227}, 285: {extraBits: 0, baseLength: 258}},
+		257: {extraBits: 0, baseLength: 3}, 258: {extraBits: 0, baseLength: 4}, 259: {extraBits: 0, baseLength: 5}, 260: {extraBits: 0, baseLength: 6}, 261: {extraBits: 0, baseLength: 7}, 262: {extraBits: 0, baseLength: 8}, 263: {extraBits: 0, baseLength: 9}, 264: {extraBits: 0, baseLength: 10}, 265: {extraBits: 1, baseLength: 11}, 266: {extraBits: 1, baseLength: 13}, 267: {extraBits: 1, baseLength: 15}, 268: {extraBits: 1, baseLength: 17}, 269: {extraBits: 2, baseLength: 19}, 270: {extraBits: 2, baseLength: 23}, 271: {extraBits: 2, baseLength: 27}, 272: {extraBits: 3, baseLength: 31}, 273: {extraBits: 3, baseLength: 35}, 274: {extraBits: 3, baseLength: 43}, 275: {extraBits: 3, baseLength: 51}, 276: {extraBits: 3, baseLength: 59}, 277: {extraBits: 4}, 278: {extraBits: 4, baseLength: 83}, 279: {extraBits: 4, baseLength: 99}, 280: {extraBits: 4, baseLength: 115}, 281: {extraBits: 5, baseLength: 131}, 282: {extraBits: 5, baseLength: 163}, 283: {extraBits: 5, baseLength: 195}, 284: {extraBits: 5, baseLength: 227}, 285: {extraBits: 0, baseLength: 258},
+	},
+	keyOrder: []int{
+		257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285,
+	},
 }
 
 var distAlphabets = DistanceAlphabets{
@@ -65,7 +88,19 @@ var distAlphabets = DistanceAlphabets{
 		extraBits    int
 		baseDistance int
 	}{
-		{extraBits: 0, baseDistance: 1}, {extraBits: 0, baseDistance: 2}, {extraBits: 0, baseDistance: 3}, {extraBits: 0, baseDistance: 4}, {extraBits: 1, baseDistance: 5}, {extraBits: 1, baseDistance: 7}, {extraBits: 2, baseDistance: 9}, {extraBits: 2, baseDistance: 13}, {extraBits: 3, baseDistance: 17}, {extraBits: 3, baseDistance: 25}, {extraBits: 4, baseDistance: 33}, {extraBits: 4, baseDistance: 49}, {extraBits: 5, baseDistance: 65}, {extraBits: 5, baseDistance: 97}, {extraBits: 6, baseDistance: 129}, {extraBits: 6, baseDistance: 193}, {extraBits: 7, baseDistance: 257}, {extraBits: 7, baseDistance: 385}, {extraBits: 8, baseDistance: 513}, {extraBits: 8, baseDistance: 769}, {extraBits: 9, baseDistance: 1025}, {extraBits: 9, baseDistance: 1537}, {extraBits: 10, baseDistance: 2049}, {extraBits: 10, baseDistance: 3073}, {extraBits: 11, baseDistance: 4097}, {extraBits: 11, baseDistance: 6145}, {extraBits: 12, baseDistance: 8193}, {extraBits: 12, baseDistance: 12289}, {extraBits: 13, baseDistance: 16385}, {extraBits: 13, baseDistance: 24577}},
+		{extraBits: 0, baseDistance: 1}, {extraBits: 0, baseDistance: 2}, {extraBits: 0, baseDistance: 3}, {extraBits: 0, baseDistance: 4}, {extraBits: 1, baseDistance: 5}, {extraBits: 1, baseDistance: 7}, {extraBits: 2, baseDistance: 9}, {extraBits: 2, baseDistance: 13}, {extraBits: 3, baseDistance: 17}, {extraBits: 3, baseDistance: 25}, {extraBits: 4, baseDistance: 33}, {extraBits: 4, baseDistance: 49}, {extraBits: 5, baseDistance: 65}, {extraBits: 5, baseDistance: 97}, {extraBits: 6, baseDistance: 129}, {extraBits: 6, baseDistance: 193}, {extraBits: 7, baseDistance: 257}, {extraBits: 7, baseDistance: 385}, {extraBits: 8, baseDistance: 513}, {extraBits: 8, baseDistance: 769}, {extraBits: 9, baseDistance: 1025}, {extraBits: 9, baseDistance: 1537}, {extraBits: 10, baseDistance: 2049}, {extraBits: 10, baseDistance: 3073}, {extraBits: 11, baseDistance: 4097}, {extraBits: 11, baseDistance: 6145}, {extraBits: 12, baseDistance: 8193}, {extraBits: 12, baseDistance: 12289}, {extraBits: 13, baseDistance: 16385}, {extraBits: 13, baseDistance: 24577},
+	},
+}
+
+var rleAlphabets = CodeLengthAlphabets{
+	alphabets: map[int]struct {
+		extraBits int
+	}{
+		16: {extraBits: 2}, 17: {extraBits: 3}, 18: {extraBits: 7}, 0: {extraBits: 0}, 8: {extraBits: 0}, 7: {extraBits: 0}, 9: {extraBits: 0}, 6: {extraBits: 0}, 10: {extraBits: 0}, 5: {extraBits: 0}, 11: {extraBits: 0}, 4: {extraBits: 0}, 12: {extraBits: 0}, 3: {extraBits: 0}, 13: {extraBits: 0}, 2: {extraBits: 0}, 14: {extraBits: 0}, 1: {extraBits: 0}, 15: {extraBits: 0},
+	},
+	keyOrder: []int{
+		16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15,
+	},
 }
 
 type CompressionWriter struct {
@@ -74,12 +109,17 @@ type CompressionWriter struct {
 type CompressionReader struct {
 	core *compressionCore
 }
+type bitBuffer struct {
+	output    io.ReadWriter
+	bitHolder uint32
+	bitCount  uint
+}
 
 type compressionCore struct {
 	isInputBufferClosed bool
 	lock                sync.Mutex
 	inputBuffer         io.ReadWriter
-	outputBuffer        io.ReadWriter
+	outputBuffer        *bitBuffer
 	btype               uint32
 	bfinal              uint32
 }
@@ -90,7 +130,7 @@ func (cr *CompressionReader) Read(data []byte) (int, error) {
 	if !cr.core.isInputBufferClosed {
 		return 0, errors.New("input buffer not closed")
 	}
-	return cr.core.outputBuffer.Read(data)
+	return cr.core.outputBuffer.output.Read(data)
 }
 
 func (cr *CompressionReader) Close() error {
@@ -128,7 +168,9 @@ func (cw *CompressionWriter) Close() error {
 
 func NewCompressionReaderAndWriter(btype uint32, bfinal uint32) (io.ReadCloser, io.WriteCloser) {
 	newCompressionCore := new(compressionCore)
-	newCompressionCore.inputBuffer, newCompressionCore.outputBuffer = new(bytes.Buffer), new(bytes.Buffer)
+	newBitBuffer := new(bitBuffer)
+	newBitBuffer.output = new(bytes.Buffer)
+	newCompressionCore.inputBuffer, newCompressionCore.outputBuffer = new(bytes.Buffer), newBitBuffer
 	newCompressionCore.isInputBufferClosed = false
 	newCompressionCore.btype = btype
 	newCompressionCore.bfinal = bfinal
@@ -137,12 +179,12 @@ func NewCompressionReaderAndWriter(btype uint32, bfinal uint32) (io.ReadCloser, 
 	return newCompressionReader, newCompressionWriter
 }
 
-func (da *DistanceAlphabets) FindCode(value int) (code int, offset int, err error) {
+func (dc *DistanceCode) FindCode(value int) (code int, offset int, err error) {
 	if value < 1 || value > maxAllowedBackwardDistance {
 		return 0, 0, errors.New("value is out of range to have a match with RFC distance code")
 	}
-	for i, info := range da.alphabets {
-		if value >= info.baseDistance && (i+1 == len(da.alphabets) || value < da.alphabets[i+1].baseDistance) {
+	for i, info := range distAlphabets.alphabets {
+		if value >= info.baseDistance && (i+1 == len(distAlphabets.alphabets) || value < distAlphabets.alphabets[i+1].baseDistance) {
 			offset := value - info.baseDistance
 			return i, offset, nil
 		}
@@ -150,7 +192,7 @@ func (da *DistanceAlphabets) FindCode(value int) (code int, offset int, err erro
 	return 0, 0, fmt.Errorf("no distance code found for the distance value %v\n", value)
 }
 
-func (da *DistanceAlphabets) Encode(items interface{}) ([]int, error) {
+func (dc *DistanceCode) Encode(items interface{}) ([]int, error) {
 	tokens, ok := items.([]Token)
 	if !ok {
 		return nil, errors.New("distance huffman tree cannot be generated without the type of Token slice")
@@ -158,7 +200,7 @@ func (da *DistanceAlphabets) Encode(items interface{}) ([]int, error) {
 	symbolFreq := make([]int, 30)
 	for _, token := range tokens {
 		if token.Kind == MatchToken {
-			if code, offset, err := da.FindCode(token.Distance); err != nil {
+			if code, offset, err := dc.FindCode(token.Distance); err != nil {
 				return nil, err
 			} else {
 				token.DistanceCode, token.DistanceOffset = code, offset
@@ -169,12 +211,25 @@ func (da *DistanceAlphabets) Encode(items interface{}) ([]int, error) {
 	if distHuffmanCode, err := huffman.BuildCanonicalHuffmanTree(symbolFreq, 15); err != nil {
 		return nil, err
 	} else {
-		da.canonicalCode = distHuffmanCode
+		dc.DistanceHuffman = distHuffmanCode
 		return findLengthBoundary(distHuffmanCode, 0), nil
 	}
 }
 
-func (la *LengthAlphabets) Encode(items interface{}) ([]int, error) {
+func (llc *LitLengthCode) FindCode(value int) (code int, offset int, err error) {
+	if value < 3 || value > maxAllowedMatchLength {
+		return 0, 0, errors.New("value is out of range to have a match with RFC length code")
+	}
+	for _, key := range lenAlphabets.keyOrder {
+		if nextInfo, ok := lenAlphabets.alphabets[key+1]; value >= lenAlphabets.alphabets[key].baseLength && (!ok || value < nextInfo.baseLength) {
+			offset := value - lenAlphabets.alphabets[key].baseLength
+			return key, offset, nil
+		}
+	}
+	return 0, 0, fmt.Errorf("no length code found for the length value %v\n", value)
+}
+
+func (llc *LitLengthCode) Encode(items interface{}) ([]int, error) {
 	tokens, ok := items.([]Token)
 	if !ok {
 		return nil, errors.New("length huffman code cannot be generated without the type of Token slice")
@@ -184,7 +239,7 @@ func (la *LengthAlphabets) Encode(items interface{}) ([]int, error) {
 		if token.Kind == LiteralToken {
 			symbolFreq[token.Value]++
 		} else {
-			if code, offset, err := la.FindCode(token.Length); err != nil {
+			if code, offset, err := llc.FindCode(token.Length); err != nil {
 				return nil, err
 			} else {
 				token.LengthCode, token.LengthOffset = code, offset
@@ -196,22 +251,130 @@ func (la *LengthAlphabets) Encode(items interface{}) ([]int, error) {
 	if litLenHuffmanCode, err := huffman.BuildCanonicalHuffmanTree(symbolFreq, 15); err != nil {
 		return nil, err
 	} else {
-		la.canonicalCode = litLenHuffmanCode
+		llc.LitLengthHuffman = litLenHuffmanCode
 		return findLengthBoundary(litLenHuffmanCode, 256), nil
 	}
 }
 
-func (la *LengthAlphabets) FindCode(value int) (code int, offset int, err error) {
-	if value < 3 || value > maxAllowedMatchLength {
-		return 0, 0, errors.New("value is out of range to have a match with RFC length code")
+func (clc *CodeLengthCode) FindCode(items interface{}) (code int, offset int, err error) {
+
+}
+
+func (clc *CodeLengthCode) Encode(items interface{}) ([]int, error) {
+	lengthHuffmanLengths, ok := items.([]int)
+	if !ok {
+		return nil, errors.New("code-length huffman code cannot be generated without the type of int slice")
 	}
-	for key, info := range la.alphabets {
-		if nextInfo, ok := la.alphabets[key+1]; value >= info.baseLength && (!ok || value < nextInfo.baseLength) {
-			offset := value - info.baseLength
-			return key, offset, nil
+	countZero, countSame := 0, 0
+	for i, length := range lengthHuffmanLengths {
+		if length == 0 {
+			if countSame != 0 {
+				if countSame < 3 {
+					for _ = range countSame {
+						clc.LengthEncoded = append(clc.LengthEncoded, struct {
+							RLECode int
+							Offset  int
+						}{
+							RLECode: lengthHuffmanLengths[i-1],
+							Offset:  0,
+						})
+					}
+				} else {
+					clc.LengthEncoded = append(clc.LengthEncoded, struct {
+						RLECode int
+						Offset  int
+					}{
+						RLECode: 16,
+						Offset:  countSame - 3,
+					})
+				}
+				countSame = 0
+			}
+			countZero++
+			if countZero == 138 {
+				clc.LengthEncoded = append(clc.LengthEncoded, struct {
+					RLECode int
+					Offset  int
+				}{
+					RLECode: 18,
+					Offset:  countZero - 11,
+				})
+				countZero = 0
+			}
+		} else if i == 0 || length != lengthHuffmanLengths[i-1] {
+			if countZero != 0 {
+				if countZero < 3 {
+					for _ = range countZero {
+						clc.LengthEncoded = append(clc.LengthEncoded, struct {
+							RLECode int
+							Offset  int
+						}{
+							RLECode: 0,
+							Offset:  0,
+						})
+					}
+				} else if countZero < 11 {
+					clc.LengthEncoded = append(clc.LengthEncoded, struct {
+						RLECode int
+						Offset  int
+					}{
+						RLECode: 17,
+						Offset:  countZero - 3,
+					})
+				} else if countZero < 139 {
+					clc.LengthEncoded = append(clc.LengthEncoded, struct {
+						RLECode int
+						Offset  int
+					}{
+						RLECode: 18,
+						Offset:  countZero - 11,
+					})
+				}
+				countZero = 0
+			}
+			if countSame != 0 {
+				if countSame < 3 {
+					for _ = range countSame {
+						clc.LengthEncoded = append(clc.LengthEncoded, struct {
+							RLECode int
+							Offset  int
+						}{
+							RLECode: lengthHuffmanLengths[i-1],
+							Offset:  0,
+						})
+					}
+				} else {
+					clc.LengthEncoded = append(clc.LengthEncoded, struct {
+						RLECode int
+						Offset  int
+					}{
+						RLECode: 16,
+						Offset:  countSame - 3,
+					})
+				}
+				countSame = 0
+			}
+			clc.LengthEncoded = append(clc.LengthEncoded, struct {
+				RLECode int
+				Offset  int
+			}{
+				RLECode: length,
+				Offset:  0,
+			})
+		} else {
+			countSame++
+			if countSame == 6 {
+				clc.LengthEncoded = append(clc.LengthEncoded, struct {
+					RLECode int
+					Offset  int
+				}{
+					RLECode: 16,
+					Offset:  countSame - 3,
+				})
+				countSame = 0
+			}
 		}
 	}
-	return 0, 0, fmt.Errorf("no length code found for the length value %v\n", value)
 }
 
 func (cw *CompressionWriter) compress(content []byte) error {
@@ -222,24 +385,30 @@ func (cw *CompressionWriter) compress(content []byte) error {
 	if err != nil {
 		return err
 	}
-	litLenHuffmanCodeLengths, err := lenAlphabets.Encode(tokens)
+	newLitLengthCode := new(LitLengthCode)
+	litLenHuffmanLengths, err := newLitLengthCode.Encode(tokens)
 	if err != nil {
 		return err
 	}
-	distHuffmanCodeLengths, err := distAlphabets.Encode(tokens)
+	newDistanceCode := new(DistanceCode)
+	distHuffmanLengths, err := newDistanceCode.Encode(tokens)
 	if err != nil {
 		return err
 	}
 	cw.core.lock.Lock()
 	defer cw.core.lock.Unlock()
-	writeCompressedContent(cw.Write, cw.core.bfinal, 1)
-	writeCompressedContent(cw.Write, cw.core.btype, 2)
-	HLIT := len(litLenHuffmanCodeLengths) - 257
-	HDIST := len(distHuffmanCodeLengths) - 1
-	writeCompressedContent(cw.core.outputBuffer.Write, uint32(HLIT), 5)
-	writeCompressedContent(cw.Write, uint32(HDIST), 5)
-	concatenatedHuffmanCodeLengths := append(litLenHuffmanCodeLengths, distHuffmanCodeLengths...)
-
+	cw.core.outputBuffer.writeCompressedContent(cw.core.bfinal, 1)
+	cw.core.outputBuffer.writeCompressedContent(cw.core.btype, 2)
+	HLIT := len(litLenHuffmanLengths) - 257
+	HDIST := len(distHuffmanLengths) - 1
+	cw.core.outputBuffer.writeCompressedContent(uint32(HLIT), 5)
+	cw.core.outputBuffer.writeCompressedContent(uint32(HDIST), 5)
+	concatenatedHuffmanLengths := append(litLenHuffmanLengths, distHuffmanLengths...)
+	newCodeLengthCode := new(CodeLengthCode)
+	codeLengthHuffmanLengths, err := newCodeLengthCode.Encode(concatenatedHuffmanLengths)
+	if err != nil {
+		return err
+	}
 }
 
 func tokeniseLZSS(refChannels []chan lzss.Reference) ([]Token, error) {
@@ -291,6 +460,30 @@ func findLengthBoundary(items []huffman.CanonicalHuffmanCode, threshold int) []i
 	return length
 }
 
-func writeCompressedContent(outputBufferWriter func([]byte) (int, error), value uint32, nbits uint) error {
+func (bb *bitBuffer) writeCompressedContent(value uint32, nbits uint) error {
+	if nbits == 0 {
+		return nil
+	}
+	trimbits := min(nbits, 32-bb.bitCount)
+	bb.bitHolder |= (value & ((1 << trimbits) - 1)) << uint32(bb.bitCount)
+	bb.bitCount += trimbits
+	for bb.bitCount >= 8 {
+		byteVal := byte(bb.bitHolder & 0xFF)
+		if _, err := bb.output.Write([]byte{byteVal}); err != nil {
+			return err
+		}
+		bb.bitHolder >>= 8
+		bb.bitCount -= 8
+	}
+	value >>= uint32(trimbits)
+	return bb.writeCompressedContent(value, nbits-trimbits)
+}
 
+func reverseBitsLoop(x uint32, n uint) uint32 {
+	rev := uint32(0)
+	for ; n > 0; n-- {
+		rev |= (x & 1) << (n - 1)
+		x >>= 1
+	}
+	return rev
 }
